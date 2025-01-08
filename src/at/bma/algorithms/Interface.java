dab5a1cff429110d59c8255d753a3fa88fa53d1f
SenfundToast <mathias.beirer@student.htldornbirn.at>
2025-01-08 01:36:12 +0100

    interface - created (feat:Add menu interface with basic structure)

--- src/at/bma/algorithms/Interface.java
+++ src/at/bma/algorithms/Interface.java
@@ -1,9 +1,35 @@
 package at.bma.algorithms;
 
+import java.util.Scanner;
+
 public class Interface {
-    int[] arrayToSort = DataGenerator.generateDataArray(5, 1, 5);
-    
+    private int[] arrayToSort = DataGenerator.generateDataArray(5, 1, 5);
+    private Scanner scanner = new Scanner(System.in);
+    private boolean isGoing = true;
+
     public void run() {
-        // Adding menu logic 
+        while (isGoing) {
+            System.out.println("\nEnter a number to make the following actions:");
+            System.out.println("1 - Choose a sorting algorithm");
+            System.out.println("2 - Execute a sorting operation");
+            System.out.println("3 - Create random list of length n");
+            
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Choose sorting algorithm (not implemented yet)");
+                    break;
+                case 2:
+                    System.out.println("Execute sorting (not implemented yet)");
+                    break;
+                case 3:
+                    System.out.println("Enter length:");
+                    int length = scanner.nextInt();
+                    System.out.println("Create random list (not implemented yet)");
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
     }
 }
